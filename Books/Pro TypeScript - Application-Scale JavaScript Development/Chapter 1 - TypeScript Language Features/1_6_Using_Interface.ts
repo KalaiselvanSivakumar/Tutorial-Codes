439cf044a789c1be9942c5288e39795247b33fe9
Kalai Selvan Sivakumar <kalaiselvan1993@ymail.com>
2017-12-22 21:40:40 +0530

    Using interface to simplify type annotation

---
+++ Books/Pro TypeScript - Application-Scale JavaScript Development/Chapter 1 - TypeScript Language Features/1_6_Using_Interface.ts
@@ -0,0 +1,12 @@
+// Using interface, type annotations can be simplified
+interface Person {
+    firstName: string,
+    lastName: string,
+    age: number
+}
+
+var person: Person = {
+    firstName: "Kalai selvan",
+    lastName: "Sivakumar",
+    age: 25
+}
