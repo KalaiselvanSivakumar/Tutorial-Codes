2ba17d9ecf37d84d8732ea5ffad4abaef3e42951
Kalai Selvan Sivakumar <kalaiselvan1993@ymail.com>
2018-01-13 22:15:13 +0530

    String to Number conversion using unary operator

---
+++ Books/Pro TypeScript - Application-Scale JavaScript Development/Chapter 1 - TypeScript Language Features/1_16_Unary_Operator_Number_Conversion.ts
@@ -0,0 +1,5 @@
+var numberedString = '5';
+var positiveNumber: number = +numberedString;
+var negativeNumber: number = -numberedString;
+console.log(positiveNumber); // Prints 5
+console.log(negativeNumber); // Prints -5
