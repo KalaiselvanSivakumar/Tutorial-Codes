341e87017bd5e178ff747ccc2aa62c0f39ee025a
Kalai Selvan Sivakumar <kalaiselvan1993@ymail.com>
2018-01-13 22:21:21 +0530

    Shorthand operator for if statement

---
+++ Books/Pro TypeScript - Application-Scale JavaScript Development/Chapter 1 - TypeScript Language Features/1_19_IF_Replacement_Using_AND_Operator.ts
@@ -0,0 +1,10 @@
+
+// longhand
+if (console) {
+    console.log("Printing from inside if statement");
+}
+
+// shorthand
+console && console.log("Printing from AND operator");
+
+// If statement can be replaced by this method, to avoid more code as well as minify js file.
