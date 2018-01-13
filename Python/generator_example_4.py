38aeec2384772e477996a9e49129df29c880ebc8
Kalai Selvan Sivakumar <kalaiselvan1993@ymail.com>
2018-01-13 22:48:23 +0530

    Assumption is wrong. Corrected the explanation

--- Python/generator_example_4.py
+++ Python/generator_example_4.py
@@ -12,17 +12,15 @@ def numberGenerator(num):
 print (list(numberGenerator(10))) # Prints [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 '''
-However when you change finally block to rint onstead of yield,
+However when you change finally block to print instead of yield,
 result will be:
 10
 [1, 2, 3, 4, 5, 6, 7, 8, 9]
 '''
 
 '''
-My assumption from the above result:
-May be generator yields all the data
-and stores it in a memory block
-and not yielding in every next() call.
-Instead of generating in every call,
-it just returns already generated output one by one.
+The above result is due to the finally block is executed
+after the completion of try block, it prints 10.
+At the time of printing, list(numberGenerator(10)) is preparing the list
+from the generated values. Since 10 is not yielded, the list will not have 10 in it.
 '''
