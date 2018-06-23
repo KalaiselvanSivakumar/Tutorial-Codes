4610144161f34ffb7a7fe937ea22c627b5547744
Kalai Selvan Sivakumar <kalaiselvan1993@ymail.com>
2018-06-23 15:57:31 +0530

    ChatApp: Save chat and get chats

--- Application/ChatApp/src/server.ts
+++ Application/ChatApp/src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import bodyParser from "body-parser";
 
 
 var app = express();
@@ -8,25 +9,56 @@ var conString = "mongodb://kalai:kalai123@localhost:27017/chatappdb";
 
 app.use(express.static(__dirname));
 
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+
+app.use(bodyParser.json());
+
 interface IChat {
 	name: string;
-	chat: string;
+	message: string;
 }
 
 interface IChatModel extends IChat, mongoose.Document { }
 
 var chatSchema = new mongoose.Schema({
 	name: String,
-	chat: String
+	message: String
 });
 
-var chats = mongoose.model<IChatModel>("Chats", chatSchema);
+var Chats = mongoose.model<IChatModel>("chats", chatSchema);
 
 mongoose.connect(conString,
 	(err) => {
 		console.log("Database connnection", err);
 	}
 );
+
+app.post('/chats', async (req, res) => {
+	console.log(req.body);
+	try {
+		var chat = new Chats(req.body);
+		await chat.save();
+		res.sendStatus(200);
+	}
+	catch (err) {
+		res.sendStatus(500);
+		console.log(err);
+	}
+});
+
+app.get('/chats', async (req, res) => {
+	console.log(req.body);
+	Chats.find({}, (error, chats) => {
+		if (error) {
+			console.log(error);
+			res.sendStatus(500);
+			return;
+		}
+		res.send(chats);
+	});
+});
 
 app.listen(3020, () => {
 	console.log('Listening in the port 3020');
