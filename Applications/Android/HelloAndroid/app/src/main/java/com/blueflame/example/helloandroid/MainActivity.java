f627393e488d71ce664066fcc5e9a477ca420ad4
Kalai Selvan Sivakumar <kalaiselvan1993@ymail.com>
2018-07-26 20:38:20 +0530

    Button addition and event handling

--- Applications/Android/HelloAndroid/app/src/main/java/com/blueflame/example/helloandroid/MainActivity.java
+++ Applications/Android/HelloAndroid/app/src/main/java/com/blueflame/example/helloandroid/MainActivity.java
@@ -2,12 +2,20 @@ package com.blueflame.example.helloandroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends Activity {
+
+    private static final String TAG = MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void nextPageButtonHandler(View view) {
+        Log.d(TAG, "In next page button handler method");
     }
 }
