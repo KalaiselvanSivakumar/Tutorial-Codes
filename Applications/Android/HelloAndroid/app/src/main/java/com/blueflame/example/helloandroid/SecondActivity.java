3fafe5a39727677f010b80ff448f117906532cfb
Kalai Selvan Sivakumar <kalaiselvan1993@ymail.com>
2018-07-26 23:18:54 +0530

    About SavedInstanceState

--- Applications/Android/HelloAndroid/app/src/main/java/com/blueflame/example/helloandroid/SecondActivity.java
+++ Applications/Android/HelloAndroid/app/src/main/java/com/blueflame/example/helloandroid/SecondActivity.java
@@ -14,10 +14,29 @@ public class SecondActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+        /**
+         * To get data from the savedInstanceState, use the below commented lines.
+         * In the below code, KEY is a constant denoting a key
+         */
+
+//        if (savedInstanceState != null) {
+//            savedInstanceState.getString(KEY);
+//            this.key = KEY;
+//        }
+
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         // The following method call should be used only after setContentView method call
         TextView textView = findViewById(R.id.selected_title);
         textView.setText(message);
     }
+
+    // To save data to the InstanceState, use the below method
+    // Ensure to save the variables before calling super function
+
+//    @Override
+//    protected void onSaveInstanceState(Bundle outState) {
+//        outState.putString(KEY, this.key);
+//        super.onSaveInstanceState(outState);
+//    }
 }
