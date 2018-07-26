40f147d18c3bea4aa4a04baadcde1a921a781a8a
Kalai Selvan Sivakumar <kalaiselvan1993@ymail.com>
2018-07-26 22:56:58 +0530

    Sending data from Activity to Another, Accessing UI element in java using id

--- Applications/Android/HelloAndroid/app/src/main/java/com/blueflame/example/helloandroid/MainActivity.java
+++ Applications/Android/HelloAndroid/app/src/main/java/com/blueflame/example/helloandroid/MainActivity.java
@@ -5,10 +5,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
     private static final String TAG = MainActivity.class.getName();
+    public static final String EXTRA_MESSAGE = "com.blueflame.example.helloandroid.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +21,10 @@ public class MainActivity extends Activity {
     public void nextPageButtonHandler(View view) {
         Log.d(TAG, "In next page button handler method");
         Intent intent = new Intent(this, SecondActivity.class);
+        Button button = (Button) view;
+        String buttonTextMessage = button.getText().toString();
+        Log.d(TAG, buttonTextMessage);
+        intent.putExtra(EXTRA_MESSAGE, buttonTextMessage);
         startActivity(intent);
     }
 }
--- Applications/Android/HelloAndroid/app/src/main/java/com/blueflame/example/helloandroid/SecondActivity.java
+++ Applications/Android/HelloAndroid/app/src/main/java/com/blueflame/example/helloandroid/SecondActivity.java
@@ -1,13 +1,23 @@
 package com.blueflame.example.helloandroid;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends Activity {
+
+    private static final String TAG = SecondActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        // The following method call should be used only after setContentView method call
+        TextView textView = findViewById(R.id.selected_title);
+        textView.setText(message);
     }
 }
