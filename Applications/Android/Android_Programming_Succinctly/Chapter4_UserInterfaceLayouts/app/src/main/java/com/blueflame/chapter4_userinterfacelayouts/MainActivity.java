2d06aaca258999aa5de3f77a9d095335f56470df
Kalai Selvan Sivakumar <kalaiselvan1993@ymail.com>
2018-07-28 16:49:26 +0530

    User Interface Layouts: ListView example and dynamic construction of it

--- Applications/Android/Android_Programming_Succinctly/Chapter4_UserInterfaceLayouts/app/src/main/java/com/blueflame/chapter4_userinterfacelayouts/MainActivity.java
+++ Applications/Android/Android_Programming_Succinctly/Chapter4_UserInterfaceLayouts/app/src/main/java/com/blueflame/chapter4_userinterfacelayouts/MainActivity.java
@@ -2,12 +2,30 @@ package com.blueflame.chapter4_userinterfacelayouts;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+
+		setContentView(R.layout.activity_listview);
+
+		// Data set for ListView
+		String[] data = new String[]
+				{
+						"Item 1", "Item 2", "Item 3", "Item 4", "Item 5"
+				};
+
+		// Data set passed to Adapter to create View objects
+		ArrayAdapter<String> adapter =
+				new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
+
+		// Passing adapter to the layout (ListView)
+        ListView listView = findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+	}
 }
