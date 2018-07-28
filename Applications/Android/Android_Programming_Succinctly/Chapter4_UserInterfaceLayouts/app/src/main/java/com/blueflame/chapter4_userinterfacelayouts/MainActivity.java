131e3ca94aae235bb3aafee79fceb5a83ce0e08b
Kalai Selvan Sivakumar <kalaiselvan1993@ymail.com>
2018-07-28 18:31:56 +0530

    User Interface Layouts: Grid View and dynamic construction

--- Applications/Android/Android_Programming_Succinctly/Chapter4_UserInterfaceLayouts/app/src/main/java/com/blueflame/chapter4_userinterfacelayouts/MainActivity.java
+++ Applications/Android/Android_Programming_Succinctly/Chapter4_UserInterfaceLayouts/app/src/main/java/com/blueflame/chapter4_userinterfacelayouts/MainActivity.java
@@ -3,6 +3,7 @@ package com.blueflame.chapter4_userinterfacelayouts;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.ListView;
 
 public class MainActivity extends Activity {
@@ -10,14 +11,25 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
+		// Normal Linear Layout
+        /*
+		setContentView(R.layout.activity_main);
+		*/
 
-		setContentView(R.layout.activity_listview);
+
+
+        // List View
+//		setContentView(R.layout.activity_listview);
+
+		// Grid View
+		setContentView(R.layout.activity_gridview);
 
 		// Data set for ListView
 		String[] data = new String[]
 				{
-						"Item 1", "Item 2", "Item 3", "Item 4", "Item 5"
+						"Item 1", "Item 2", "Item 3",
+						"Item 4", "Item 5", "item 6",
+						"Item 7", "Item 8", "item 9"
 				};
 
 		// Data set passed to Adapter to create View objects
@@ -25,7 +37,13 @@ public class MainActivity extends Activity {
 				new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
 
 		// Passing adapter to the layout (ListView)
-        ListView listView = findViewById(R.id.listView);
-        listView.setAdapter(adapter);
+		// List View
+//        ListView listView = findViewById(R.id.listView);
+//        listView.setAdapter(adapter);
+
+		// Grid View
+		GridView gridView = findViewById(R.id.gridView);
+		gridView.setAdapter(adapter);
+
 	}
 }
