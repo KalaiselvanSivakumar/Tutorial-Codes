6803335d29f4b86a6a0a4479c29389f988f0c74d
Kalai Selvan Sivakumar <kalaiselvan1993@ymail.com>
2018-07-29 18:47:33 +0530

    User Interface Widgets: ImageView static and dynamic source setting

--- Applications/Android/Android_Programming_Succinctly/Chapter5_UserInterfaceWidgets/app/src/main/java/com/blueflame/userinterfacewidgets/MainActivity.java
+++ Applications/Android/Android_Programming_Succinctly/Chapter5_UserInterfaceWidgets/app/src/main/java/com/blueflame/userinterfacewidgets/MainActivity.java
@@ -1,13 +1,24 @@
 package com.blueflame.userinterfacewidgets;
 
 import android.app.Activity;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_imageview);
+
+		// Image View - Image source setting programmatically
+		ImageView imageView = findViewById(R.id.imageView);
+		Resources resources = getResources();
+		Drawable image = resources.getDrawable(R.drawable.ic_launcher_foreground);
+		// Uncomment below line to set different image in the ImageView
+//		imageView.setImageDrawable(image);
+
 	}
 }
