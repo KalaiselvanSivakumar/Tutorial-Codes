3cec36bd064fcb21d866c1406065a39c7e4f44ce
Kalai Selvan Sivakumar <kalaiselvan1993@ymail.com>
2018-07-29 21:04:58 +0530

    User Interface Widgets: Editable Text and analysing user input data

--- Applications/Android/Android_Programming_Succinctly/Chapter5_UserInterfaceWidgets/app/src/main/java/com/blueflame/userinterfacewidgets/MainActivity.java
+++ Applications/Android/Android_Programming_Succinctly/Chapter5_UserInterfaceWidgets/app/src/main/java/com/blueflame/userinterfacewidgets/MainActivity.java
@@ -4,9 +4,16 @@ import android.app.Activity;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+	private static final String TAG = MainActivity.class.getName();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +30,27 @@ public class MainActivity extends Activity {
 //		imageView.setImageDrawable(image);
 		*/
 
+		// Button types
+		/*
 		setContentView(R.layout.activity_button);
+		*/
+
+		// Text View
+		setContentView(R.layout.activity_textview);
+		// Getting the text from EditText
+		EditText editText = findViewById(R.id.editText);
+		editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+			@Override
+			public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+				// EditorInfo has lots of constant to check actionId
+				if (actionId == EditorInfo.IME_ACTION_SEND) {
+					String input = v.getText().toString();
+					Log.d(TAG, String.format("Processing input: %s", input));
+				}
+				// Return false - default behaviour. If it returns false, onscreen keyboard will close
+				// If it is changed to true, then everything has to be handled manually
+				return false;
+			}
+		});
 	}
 }
