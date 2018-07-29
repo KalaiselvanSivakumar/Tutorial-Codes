59cb938c793cb7336a3bf22a2c08581887528d9f
Kalai Selvan Sivakumar <kalaiselvan1993@ymail.com>
2018-07-29 21:26:12 +0530

    User Interface Widgets: Checkbox UI and Handling

--- Applications/Android/Android_Programming_Succinctly/Chapter5_UserInterfaceWidgets/app/src/main/java/com/blueflame/userinterfacewidgets/MainActivity.java
+++ Applications/Android/Android_Programming_Succinctly/Chapter5_UserInterfaceWidgets/app/src/main/java/com/blueflame/userinterfacewidgets/MainActivity.java
@@ -2,11 +2,16 @@ package com.blueflame.userinterfacewidgets;
 
 import android.app.Activity;
 import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.KeyEvent;
+import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -14,6 +19,8 @@ import android.widget.TextView;
 public class MainActivity extends Activity {
 
 	private static final String TAG = MainActivity.class.getName();
+
+	private boolean isGreen, isBold, isBig;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +42,7 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_button);
 		*/
 
+		/*
 		// Text View
 		setContentView(R.layout.activity_textview);
 		// Getting the text from EditText
@@ -52,5 +60,51 @@ public class MainActivity extends Activity {
 				return false;
 			}
 		});
+		*/
+
+		setContentView(R.layout.activity_checkbox);
+		isGreen = false;
+		isBold = false;
+		isBig = false;
+		synchronizeCheckBoxes();
+		synchronizeTextView();
+
+	}
+
+	private void synchronizeCheckBoxes() {
+		CheckBox greenCheckBox = findViewById(R.id.greenCheckBox);
+		CheckBox boldCheckBox = findViewById(R.id.boldCheckBox);
+		CheckBox bigCheckBox = findViewById(R.id.bigCheckBox);
+		greenCheckBox.setChecked(isGreen);
+		boldCheckBox.setChecked(isBold);
+		bigCheckBox.setChecked(isBig);
+	}
+
+	private void synchronizeTextView() {
+		TextView textView = findViewById(R.id.checkBoxText);
+		String colour = isGreen ? "#FF009900" : "#FF000000";
+
+		textView.setTextColor(Color.parseColor(colour));
+		textView.setTypeface(
+				Typeface.create(Typeface.DEFAULT,
+						isBold ? Typeface.BOLD : Typeface.NORMAL));
+		textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, isBig ? 30 : 18);
+	}
+
+	public void checkBoxClickHandler(View view) {
+		CheckBox checkBox = (CheckBox) view;
+		boolean isChecked = checkBox.isChecked();
+		switch (checkBox.getId()) {
+			case R.id.greenCheckBox:
+				isGreen = isChecked;
+				break;
+			case R.id.boldCheckBox:
+				isBold = isChecked;
+				break;
+			case R.id.bigCheckBox:
+				isBig = isChecked;
+				break;
+		}
+		synchronizeTextView();
 	}
 }
