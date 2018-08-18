ab3f41fc18012ca918892c8651d181881e3b9684
Kalai Selvan Sivakumar <kalaiselvan1993@ymail.com>
2018-08-18 17:45:51 +0530

    Android Programming Succinctly: Chapter 7 - Application Data: Internal Storage example

--- Applications/Android/Android_Programming_Succinctly/Chapter7ApplicationData/app/src/main/java/com/blueflame/chapter7_applicationdata/MainActivity.java
+++ Applications/Android/Android_Programming_Succinctly/Chapter7ApplicationData/app/src/main/java/com/blueflame/chapter7_applicationdata/MainActivity.java
@@ -8,9 +8,16 @@ import android.view.KeyEvent;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class MainActivity extends Activity {
 
 	private static String SHARED_PREFS_KEY = "existingInput";
+
+	// Not proper
+	private static String FILENAME = "internalStorage";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +35,30 @@ public class MainActivity extends Activity {
 				// Logging the current key
 				String value = getPreferences(MODE_PRIVATE).getString(SHARED_PREFS_KEY, "No saved data");
 				Log.d(MainActivity.class.getName(), value);
+
+				FileInputStream inputStream = null;
+				try {
+					inputStream = openFileInput(FILENAME);
+					int maxBytes = inputStream.available();
+					byte[] data = new byte[maxBytes];
+					inputStream.read(data, 0, maxBytes);
+					while (inputStream.read(data) != -1) {}
+					String existingInput = new String(data);
+					Log.d(MainActivity.class.getName(), "Internal Storage: " + existingInput);
+				}
+				catch (IOException io) {
+					io.printStackTrace();
+				}
+				finally {
+					if (inputStream != null) {
+						try {
+							inputStream.close();
+						}
+						catch (IOException io) {
+							io.printStackTrace();
+						}
+					}
+				}
 				return false;
 			}
 		});
@@ -39,6 +70,7 @@ public class MainActivity extends Activity {
 
 	}
 
+	// Shared Preferences
 	public void saveStringWithSharedPreferences(String key, String value) {
 		SharedPreferences prefs = getPreferences(MODE_PRIVATE);
 		SharedPreferences.Editor editor = prefs.edit();
@@ -46,4 +78,14 @@ public class MainActivity extends Activity {
 		editor.putString(key, value);
 		editor.apply();
 	}
+
+	// Internal Storage
+	public void saveStringWithInternalStorage(String filename, String value)
+			throws IOException {
+		FileOutputStream outputStream = openFileOutput(filename, MODE_PRIVATE);
+		byte[] data = value.getBytes();
+		outputStream.write(data);
+		outputStream.close();
+	}
+
 }
