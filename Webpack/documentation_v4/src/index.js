41642ddb0770c0ed2cb2d7caf75e95c38c8d3918
Kalai Selvan Sivakumar <kalaiselvan1993@ymail.com>
2019-12-08 19:45:10 +0530

    Asset management webpack CSS loader and file laoder examples

--- Webpack/documentation_v4/src/index.js
+++ Webpack/documentation_v4/src/index.js
@@ -1,8 +1,23 @@
 import _ from 'lodash';
+import './css/index.css';
+
+// Note: Importing log file in this file will only replace the path of set using src attribute.
+// This import is not necessary to replace it in the CSS file url function.
+import Logo from './images/logo.png';
 
 function createComponent() {
     const element = document.createElement('div');
     element.innerHTML = _.join(['Hello', 'Webpack'], ' ');
+    // Classes are not imported. You don't have to import css file to use the classes defined in it.
+    // Including CSS file will make the webpack to include it as dependency,
+    // so that css file will be loaded by the browser when this module loads.
+    element.classList.add('hello');
+
+    // FIle loader
+    const image = new Image();
+    image.src = Logo;
+    element.appendChild(image);
+
     return element;
 }
 
--- Webpack/documentation_v4/webpack.config.js
+++ Webpack/documentation_v4/webpack.config.js
@@ -2,8 +2,25 @@ const path = require('path');
 
 module.exports = {
     entry: './src/index.js',
+    module: {
+        rules: [
+            {
+                test: /.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ],
+            },
+            {
+                test: /.(png|jpg|svg|gif)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+        ],
+    },
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
 };
