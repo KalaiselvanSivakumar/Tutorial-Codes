9b229f064388ba2eb27564ad166a675dd8511a12
Kalai Selvan Sivakumar <kalaiselvan1993@ymail.com>
2019-12-08 20:09:09 +0530

    Font loading webpack

--- Webpack/documentation_v4/webpack.config.js
+++ Webpack/documentation_v4/webpack.config.js
@@ -17,6 +17,12 @@ module.exports = {
                     'file-loader',
                 ],
             },
+            {
+                test: /.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader',
+                ]
+            },
         ],
     },
     output: {
