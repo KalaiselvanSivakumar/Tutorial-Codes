959671c1c0e3864bc3fa4180394d2b04271cbbc4
Kalai selvan Sivakumar <kalaiselvansivakumar@gmail.com>
2021-03-17 20:34:20 +0530

    Ember Component: Component State and Actions

---
+++ Ember/official/core-concepts/app/components/double-it.js
@@ -0,0 +1,17 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+
+export default class DoubleItComponent extends Component {
+  @tracked multiple = 1;
+
+  @action
+  double() {
+    this.multiple = this.multiple * 2;
+  }
+
+  @action
+  updateMultiple(amount) {
+    this.multiple = amount;
+  }
+}
